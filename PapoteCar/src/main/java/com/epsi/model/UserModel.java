e7669e5685b4f69395abaf6a59c482c73709e38f
Jcoub1 <jerome.coubronne@gmail.com>
2018-10-25 09:43:14 +0200

    package init

---
+++ PapoteCar/src/main/java/com/epsi/model/UserModel.java
@@ -0,0 +1,12 @@
+package com.epsi.model;
+
+public class UserModel {
+
+	private String nom;
+	private String prenom;
+	private String addresse;
+	private String rib;
+	private String numeroCb;
+	private String mail;
+	private String mdp;
+}
